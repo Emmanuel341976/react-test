a7d2415cd2a32efed3376f32537942db47162ff1
Emmanuel341976 <53331945+Emmanuel341976@users.noreply.github.com>
2025-05-23 09:16:22 +0200

    add pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,32 @@
 import Image from "next/image";
-import bureau from '../../public/bureau.png'
-
-
 
 export default function Home() {
 
-  
-  
-const windowdimensions = [window.innerWidth, window.innerHeight];
 
- 
-
-  console.log(windowdimensions)
-  console.log('coucou')
-
-  return (    
-    <div className="flex flex-col items-center p-8">
-
-       <h1 className="text-3xl text-red-500">Escape from the office !</h1>
-       <p>{13}</p>
-          <Image className="bureau w-[800px] h-[600px] p-8 bg-zinc-100"
-                   src={bureau}
-                   alt="Un bureau" 
+  return (
+    <div className="flex flex-col font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-center text-6xl p-4 italic font-semibold">Escape if you can...</h1>
+        <div className="justify-items-center content-center w-full">
+          <Image
+            className=""
+            src="/hall.png"
+            alt="bureau"
+            width={800}
+            height={200}
           />
+        </div> 
+      <div className="text-center text-2xl italic p-4">
+        <p>{"Now you're locked in a big, spooky house!"}</p>
+        <p>{"She told you not to enter. Now you've got to escape!"}</p>  
+        <p>{"Will you be perceptive enough to solve the riddles?"}</p>
+        <p>{"It's up to you!"}</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <button className="bg-amber-600 p-2 max-w-fit rounded-xl">Start if you dare!</button>
+      </div>
+     
+
+
     </div>
-    // <div className="flex flex-col p-8 max-h-full bg-red-100 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    //   <div className="flex flex-col p-8 max-h-1/5 bg-blue-100">
-    //     <h1 className="flex flex-col  p-8 text-3xl text-red-500">Escape from the office !</h1>
-    //   </div>
-    //   <div className="flex flex-col  p-8 max-w-fit max-h-fit bg-green-100">
-    //     <Image className="bureau"
-    //               src={bureau}
-    //               alt="Un bureau" 
-    //             />
-    //   </div>
-        
-      
-    // </div>
   );
 }
